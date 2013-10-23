eb4a639ccc105b0072a093015374337138c4bf69
weijye <weijye@cisco.com>
2013-10-23 04:06:36 -0400

    add

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -11,6 +11,8 @@ urlpatterns = patterns('',
                          {'document_root':STATICFILES_DIRS[0]+'/css'}),
     (r'^js/(?P<path>.*)$','django.views.static.serve',  
                          {'document_root':STATICFILES_DIRS[0]+'/js'}),
+     (r'^images/(?P<path>.*)$','django.views.static.serve',  
+                         {'document_root':STATICFILES_DIRS[0]+'/images'}),
     # url(r'^mysite/', include('mysite.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
